b5245463ea432ddf722280025c8886675f4b93b9
Sky <802526@seq.org>
2020-01-29 21:39:11 -0800

    Boids Art Code is Complete

--- Lab128 Art 1/boids.js
+++ Lab128 Art 1/boids.js
@@ -1,4 +1,4 @@
-class Boids {
+class Boid {
   constructor(x, y, dx, dy) {
   this.loc = createVector(x, y);
   this.vel = createVector(dx, dy);
@@ -13,18 +13,35 @@ class Boids {
   }
 
   update() {
-    this.vel = this.vel + this.acc
-    this.loc = this.loc + this.vel
-    this.vel < 10
+    this.vel.add(this.acc)
+    this.loc.add(this.vel)
   }
 
   checkEdges() {
-
+    if(this.loc.x<0){
+      this.vel.x=-1*this.vel.x;
+    }//left side of screen
+    if(this.loc.x>width){
+      this.vel.x = -1*this.vel.x;
+    }//right side of screen
+    if(this.loc.y<0){
+      this.vel.y=-1*this.vel.y;
+    }//top of the screen
+    if(this.loc.y>height){
+      this.vel.y=-1*this.vel.y;
+    }
   }
 
   render() {
-    fill(this.clr)
-
+    for(var i=0; i<boids.length; i++){
+      let d = dist(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
+      if(d < 200) {
+        stroke(this.clr, 50)
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
+      }
+    }
+    // fill(255, 135, 255)
+    // ellipse(this.loc.x, this.loc.y, 10, 10)
   }
 
 }
--- Lab128 Art 1/sketch.js
+++ Lab128 Art 1/sketch.js
@@ -7,16 +7,20 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
-  loadBoids();
+  loadBoids(50);
 
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-  boids.run();
+  background(5, 5, 5)
+  for(var i=0; i<boids.length; i++){
+  boids[i].run();
+  }
 }
 
-function loadBoids() {
-
+function loadBoids(n) {
+  for(var i=0; i<n; i++){
+    boids[i] = new Boid(random(width), random(height), random(-4, 4), random(-4, 4))
+  }
 }
