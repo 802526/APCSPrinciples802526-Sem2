8d638867ad0b4b38285c670dce1ec4350c09e062
802526 <54591152+802526@users.noreply.github.com>
2020-01-30 08:20:06 -0800

    boid stuff

---
+++ Lab130 Art2/boid.js
@@ -0,0 +1,47 @@
+class Boid {
+  constructor(x, y, dx, dy) {
+  this.loc = createVector(x, y);
+  this.vel = createVector(dx, dy);
+  this.acc = createVector(0,0);
+  this.clr = color(random(0, 256), random(0, 256), random(0, 256))
+  }
+
+  run() {
+    this.update();
+    this.checkEdges();
+    this.render();
+  }
+
+  update() {
+    this.vel.add(this.acc)
+    this.loc.add(this.vel)
+  }
+
+  checkEdges() {
+    if(this.loc.x<0){
+      this.vel.x=-1*this.vel.x;
+    }//left side of screen
+    if(this.loc.x>width){
+      this.vel.x = -1*this.vel.x;
+    }//right side of screen
+    if(this.loc.y<0){
+      this.vel.y=-1*this.vel.y;
+    }//top of the screen
+    if(this.loc.y>height){
+      this.vel.y=-1*this.vel.y;
+    }
+  }
+
+  render() {
+    for(var i=0; i<boids.length; i++){
+      let d = dist(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
+      if(d < 200) {
+        stroke(this.clr, 50)
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y)
+      }
+    }
+    fill(255, 135, 255)
+    ellipse(this.loc.x, this.loc.y, 10, 10)
+  }
+
+}
--- Lab130 Art2/sketch.js
+++ Lab130 Art2/sketch.js
@@ -1,17 +1,26 @@
-//  Your Name
-// 	Date or version number
+//  Sky Gastinel
+// 30 January 2020
 //  This is a comment
 //  The setup function function is called once when your program begins
+boids = []
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
-
+  loadBoids(20);
 
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
+  background(5, 5, 5)
+  for(var i=0; i<boids.length; i++){
+  boids[i].run();
+  }
+}
 
+function loadBoids(n) {
+  for(var i=0; i<n; i++){
+    boids[i] = new Boid(random(width), random(height), random(-4, 4), random(-4, 4))
+  }
 }
